695b7654336c12be99dcc1f9d03a55395c4e5464
Madelyn Nelson <mnelso12@nd.edu>
2017-03-04 23:46:02 -0500

    added content to 5-8

--- scripts/materialize.js
+++ scripts/materialize.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
 	$('.parallax').parallax();
+
+	var options = [
+		{selector: '#categories', offset: 50, callback: function(el) {
+			Materialize.showStaggeredList($(el));
+		} },
+
+		{selector: '#books-list', offset: 1000, callback: function(el) {
+			Materialize.showStaggeredList($(el));
+		} },
+
+		{selector: '#computational-thinking-list', offset: 1500, callback: function(el) {
+			Materialize.showStaggeredList($(el));
+		} }
+	];
+	Materialize.scrollFire(options);
 });
+
+
