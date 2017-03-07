63a0ccda5123c565916430ef6fa30b27ab289c58
Madelyn Nelson <mnelso12@nd.edu>
2017-03-06 20:12:19 -0500

    added contact us modals, changed text color of age group template

--- scripts/materialize.js
+++ scripts/materialize.js
@@ -1,16 +1,17 @@
 $(document).ready(function(){
 	$('.parallax').parallax();
+	$('.modal').modal();
 
 	var options = [
 		{selector: '#categories', offset: 50, callback: function(el) {
 			Materialize.showStaggeredList($(el));
 		} },
 
-		{selector: '#books-list', offset: 1000, callback: function(el) {
+		{selector: '#books-list', offset: 50, callback: function(el) {
 			Materialize.showStaggeredList($(el));
 		} },
 
-		{selector: '#computational-thinking-list', offset: 1500, callback: function(el) {
+		{selector: '#computational-thinking-list', offset: 50, callback: function(el) {
 			Materialize.showStaggeredList($(el));
 		} }
 	];
