2c33df15842cdf5090adc499a2c74c67b4a3cca7
Madelyn Nelson <mnelso12@nd.edu>
2017-03-22 02:25:18 -0400

    google charts of average earnings

---
+++ scripts/googleCharts.js
@@ -0,0 +1,52 @@
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+	var factor = 1000000;
+
+	var cs_style="stroke-color: #871B47; stroke-opacity: 0.6; stroke-width: 8; fill-color: #BC5679; fill-opacity: 0.2"
+	var college_style="stroke-color: #871B47; stroke-opacity: 0.6; stroke-width: 8; fill-color: #BC5679; fill-opacity: 0.2"
+	var high_school_style ="stroke-color: #871B47; stroke-opacity: 0.6; stroke-width: 8; fill-color: #BC5679; fill-opacity: 0.2"
+
+	var data = google.visualization.arrayToDataTable([
+		['', 'Average Starting Salaries', { role: 'style' }],
+		['CS Graduate', 61321, cs_style],
+		['College Graduate', 50556, college_style],
+		['High School Graduate', 0, high_school_style]
+	]);
+
+	var lifetime_data = google.visualization.arrayToDataTable([
+		['', '', { role: 'style' }],
+		['CS Graduate', 1.67*factor, cs_style],
+		['College Graduate', 1.19*factor, college_style], 
+		['High School Graduate', .58*factor, high_school_style]
+	]);
+
+	var background_color = '#c5cae9';
+
+	var options = {
+		chart: {
+			title: '',
+			subtitle: '',
+		},
+		backgroundColor: background_color,
+		legend: {position: 'none'},
+		bars: 'horizontal' // Required for Material Bar Charts.
+	};
+
+	var lifetime_options = {
+		chart: {
+			title: '',
+			subtitle: '',
+		},
+		backgroundColor: background_color,
+		legend: {position: 'none'},
+		bars: 'horizontal' // Required for Material Bar Charts.
+	};
+
+	var chart = new google.visualization.BarChart(document.getElementById('barchart_material'));
+	var lifetime_chart = new google.visualization.BarChart(document.getElementById('lifetime_chart'));
+
+	chart.draw(data, options);
+	lifetime_chart.draw(lifetime_data, lifetime_options);
+}
